53c3f74f63235ef8d030a94205276233e2556958
Hadi khaled Abdo <87321076+Hadikhaled@users.noreply.github.com>
2024-03-04 18:24:40 +0200

    Hady

---
+++ Telcom_Project/Telcom_Project/04_Triggers.sql
@@ -0,0 +1,14 @@
+--Hadi--
+--calculate duration 
+CREATE TRIGGER trgCalculteDuration 
+ON [dbo].[Call_Customer_Agent]
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE cca
+    SET cca.[Duration] = DATEDIFF(minute, i.[call_time], i.[end_call_time])
+    FROM [dbo].[Call_Customer_Agent] cca
+    INNER JOIN inserted i ON cca.call_id = i.call_id;
+END;
